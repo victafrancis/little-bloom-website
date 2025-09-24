83ad2f3a8d69585076217de4bdb1270b52354923
Francis Victa <francisvicta45@gmail.com>
2025-09-24 09:24:18 -0400

    added Vercel analytics package

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Analytics } from '@vercel/analytics/react';
 import { Header } from './src/components/Header';
 import { Footer } from './src/components/Footer';
 import { AppRoutes } from './src/routes/router';
 export function App() {
   return <HelmetProvider>
       <BrowserRouter>
+        <Analytics />
         <div className="min-h-screen flex flex-col">
           <Header />
           <div className="flex-grow">
